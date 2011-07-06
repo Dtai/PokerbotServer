920880d4b535be951d9464c24dabdd52f7fd0653
Kwinten Missiaen <kwinten.missiaen@student.kuleuven.be>
2011-07-06 15:00:59 +0200

    Eerste prototype van een server die tafels aanmaakt, spelers toevoegt en kan antwoorden met gegevens over een tafel. Eenvoudig testprogramma dat zo'n scenario doorloopt.

--- TestServer/src/Test.java
+++ TestServer/src/Test.java
@@ -1,15 +1,21 @@
 import java.io.BufferedReader;
+
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
+
+import org.apache.commons.io.IOUtils;
 
 
 public class Test {
 	
-	public static void main(String[] args) throws InterruptedException{
+	public static void main(String[] args) throws InterruptedException, IOException{
 		PrintWriter out = null;
+		InputStream inputStream = null;
 		Socket socket = null;
 		
 		//Create socket connection
@@ -53,19 +59,55 @@ public class Test {
 
 			
 			
-		//out.println(startTable);
-		//out.close();
-		System.out.println("flushed");
+		out.println(startTable);
+		out.close();
+		System.out.println("inputstream closed");
 		
-		/*Thread.sleep(15000);
+		Thread.sleep(15000);
 		System.out.println("waking up");
 		
-		try{
-			  out = new PrintWriter(socket.getOutputStream(), true);
-		}	
-		catch(Exception e){}*/
+			  try {
+				  socket = new Socket("localhost", 20000);
+				out = new PrintWriter(socket.getOutputStream(), true);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		
 		out.println(addBot);
+		
+		out.close();
+		System.out.println("inputstream closed");
+		
+		Thread.sleep(15000);
+		System.out.println("waking up");
+		
+			  try {
+				  socket = new Socket("localhost", 20000);
+				out = new PrintWriter(socket.getOutputStream(), true);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		
+		out.println(fetchData);
+		socket.shutdownOutput();
+		
+		try {
+			//socket = new Socket("localhost", 20000);
+			inputStream = socket.getInputStream();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		String reply = IOUtils.toString(inputStream);
+		
+		inputStream.close();
+		
+		System.out.println(reply);
+		
+		
 	}
 
 

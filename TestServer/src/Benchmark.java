2a4ff860368740aaa4c31a973108aa243e6a805c
Kwinten Missiaen <kwinten.missiaen@student.kuleuven.be>
2011-09-23 13:47:22 +0200

    Updated benchmark class. Removed some console output.

--- TestServer/src/Benchmark.java
+++ TestServer/src/Benchmark.java
@@ -1,33 +1,69 @@
-import java.io.IOException;
-import java.io.InputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.net.UnknownHostException;
 import java.util.Scanner;
 
 
 public class Benchmark {
-		
+			
 	static Scanner scan = new Scanner(System.in);
 	
-	final static int nbPlayers = scan.nextInt();
+	static int nbPlayers;
+	static int rulesUsed;
+	static String tableName;
 	
-	final static String ruleSet =
+	static String ruleSet;
+	final static String ruleSetBench =
 		"do(raise(X0), 1) :- X0 is 10, rank(XRval), X1 =XRval, rank(XRval), X2 =XRval, handkaarten(X3), members([card(X1, _), card(X2, _)],X3). do(raise(X0), 2) :- X0 is 5, rank(XRval), X1 =XRval, rank(XRval), X2 =XRval, allekaarten(X3), members([card(X1, _), card(X2, _)],X3). do(fold, 3) :- tekort(X0), X1 is 6, (X0 > X1). do(call, 4) :- true.";
+	final static String ruleSetPair = 
+		"do(fold, 1) :- rank(XRval), X0 =XRval, rank(XRval), X1 =XRval, allekaarten(X2), members([card(X0, _), card(X1, _)],X2).";
+	final static String ruleSetStraightFlush =
+		"do(fold, 1) :- suit(XScolor), X0 = XScolor, rank(XRval), X1 =XRval, suit(XScolor), X2 = XScolor, rank(XRval), suit(XScolor), X4 = XScolor, rank(XRval), suit(XScolor), X6 = XScolor, rank(XRval), suit(XScolor), X8 = XScolor, rank(XRval), allekaarten(X10), members([card(X1, X0), card(X3, X2), card(X5, X4), card(X7, X6), card(X9, X8)],X10), X3 =:=XRval+1, X5 =:=XRval+2, X7 =:=XRval-1, X9 =:=XRval-2.";
+	final static String ruleSetAceHigh = 
+		"do(fold, 1) :- X0 =14, allekaarten(X1), members([card(X0, _)],X1).";
+	final static String ruleSetBankroll = 
+		"do(fold, 1) :- saldo(X0), X1 is 10, (X0 > X1).";
+	final static String ruleSetFold = 
+		"do(fold,1):- true.";
 
-	public static void main(String args[]) throws UnknownHostException, IOException, InterruptedException{
+	public static void main(String args[]) throws Exception{
+		
+		System.out.println("How many players?");
+		nbPlayers = scan.nextInt();
+		System.out.println("What rulesset?");
+		System.out.println("1) benchmark");
+		System.out.println("2) pair");
+		System.out.println("3) straight flush");
+		System.out.println("4) ace high");
+		System.out.println("5) bankroll > 10");
+		System.out.println("6) fold");
+		rulesUsed = scan.nextInt();
+		
+		System.out.println("Table name?");
+		tableName = scan.next();
 		
 		final String startTableBench = 				
-				"{\"request\": {" + 
-				     "\"type\": \"startTable\"," +  
-				     "\"tableName\": \"bench\" ," +
-				     "\"nbPlayers\": " +nbPlayers +" ," +
-				     "\"password\": \"bench\"" +
-				     "}" +
-				"}";
+			"{\"request\": {" + 
+			     "\"type\": \"startTable\"," +  
+			     "\"tableName\": \"" + tableName +"\" ," +
+			     "\"nbPlayers\": " + nbPlayers +" ," +
+			     "\"password\": \"bench\"" +
+			     "}" +
+			"}";
+		
+		switch(rulesUsed){
+		case 1: ruleSet = ruleSetBench; break;
+		case 2: ruleSet = ruleSetPair; break;
+		case 3: ruleSet = ruleSetStraightFlush; break;
+		case 4: ruleSet = ruleSetAceHigh; break;
+		case 5: ruleSet = ruleSetBankroll;break;
+		case 6: ruleSet = ruleSetFold; break;
+		
+		default: 
+			System.out.println("Please enter a number between 1 and 6.");
+			throw new Exception("Please enter a number between 1 and 6.");
+		}
 
 		PrintWriter out = null;
-		InputStream inputStream = null;
 		Socket socket = null;
 
 		//Create socket connection
@@ -40,7 +76,7 @@ public class Benchmark {
 		out.close();
 		System.out.println("Inputstream closed. Table should be started.");
 		
-		Thread.sleep(3000);
+		Thread.sleep(5000);
 		
 		//Add players
 		for(int i=0; i<nbPlayers; i++){
@@ -54,12 +90,12 @@ public class Benchmark {
 		
 		//Set up a test table for every person
 		
-		for(int i=0; i<nbPlayers; i++){
-			socket = new Socket("borgraf", 20000);
-			out = new PrintWriter(socket.getOutputStream(), true);
-			out.print(setupTesttable(i));
-			out.close();
-		}
+//		for(int i=0; i<nbPlayers; i++){
+//			socket = new Socket("borgraf", 20000);
+//			out = new PrintWriter(socket.getOutputStream(), true);
+//			out.print(setupTesttable(i));
+//			out.close();
+//		}
 
 	}
 	
@@ -68,7 +104,7 @@ public class Benchmark {
 		return
 			"{\"request\": { " +
 		    "\"type\": \"joinTable\"," +
-		    "\"tableName\":\"bench\" ," +
+		    "\"tableName\":\"" + tableName +"\" ," +
 		    "\"playerName\": \"" + playerName +"\" ,"+
 		    "\"description\": \"" + ruleSet + "\"" +
 		    "}" +

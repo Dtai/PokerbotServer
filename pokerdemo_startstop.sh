cc94b260f3b4af7960e9551f413f53d3ff298488
Tias Guns <tias.guns@cs.kuleuven.be>
2016-08-03 10:05:42 +0200

    pokerdemo_startstop: log saving on stop/restart

--- pokerdemo_startstop.sh
+++ pokerdemo_startstop.sh
@@ -4,8 +4,10 @@
 # Based on template from http://werxltd.com/wp/2012/01/05/simple-init-d-script-template/
 # Tias, 2012-09-07
 
-HOME="/home/pokerdemo"
+#HOME="/home/pokerdemo"
 DAEMON_PATH="$HOME/PokerbotServer/opentestbed"
+LOGDIR="$HOME/logs"
+LOGGING=true
 
 DAEMON=ant
 DAEMONOPTS="PrologBotServer.1"
@@ -14,14 +16,13 @@ NAME=pokerdemo
 DESC="Poker demo app, backend"
 PIDFILE="$HOME/$NAME.pid"
 #SCRIPTNAME=/etc/init.d/$NAME
-DEBUG=false
 
 case "$1" in
 start)
 	printf "%-50s" "Starting $NAME..."
 	cd $DAEMON_PATH
 	if $DEBUG; then
-		PID=`nohup $DAEMON $DAEMONOPTS < /dev/null >/tmp/$NAME.out 2>/tmp/$NAME.err & echo $!`
+		PID=`nohup $DAEMON $DAEMONOPTS < /dev/null >$LOGDIR/$NAME.out 2>$LOGDIR/$NAME.err & echo $!`
 	else
 		PID=`nohup $DAEMON $DAEMONOPTS < /dev/null > /dev/null 2>&1 & echo $!`
 	fi
@@ -35,6 +36,7 @@ start)
 ;;
 status)
         printf "%-50s" "Checking $NAME..."
+        cd $DAEMON_PATH
         if [ -f $PIDFILE ]; then
             PID=`cat $PIDFILE`
             if [ -z "`ps axf | grep ${PID} | grep -v grep`" ]; then
@@ -48,9 +50,11 @@ status)
 ;;
 stop)
         printf "%-50s" "Stopping $NAME"
-            PID=`cat $PIDFILE`
-            cd $DAEMON_PATH
+        now=$(date +%Y-%m-%d--%H-%M)
+        cp $LOGDIR/$NAME.out $LOGDIR/log-$now.out
+        cd $DAEMON_PATH
         if [ -f $PIDFILE ]; then
+            PID=`cat $PIDFILE`
             kill $PID
             printf "%s\n" "Ok"
             rm -f $PIDFILE
